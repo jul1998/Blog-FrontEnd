7bd534cf91fa3dd870c0368352e4407499e6675c
jul1998 <jguevara321@gmail.com>
2023-01-26 16:14:52 -0600

    added text editor success

--- src/front/js/component/Post Comp/postsComp.jsx
+++ src/front/js/component/Post Comp/postsComp.jsx
@@ -2,15 +2,17 @@ import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../../store/appContext";
 import Header from "../header.jsx";
 import postbg from "../../../img/post-bg.jpg";
+import { Link } from "react-router-dom";
 
 
 function Posts({postId}){
     const { store, actions } = useContext(Context);
     const [content, setContent] = useState([])
+    let token = localStorage.getItem("token")
 
     useEffect(()=>{
         async function fetch() {
-            let response = await actions.genericFetch(`post/${postId}`)
+            let response = await actions.genericFetchProtected(`post/${postId}`)
             let jsonResponse = await response.json()
             setContent(jsonResponse);
             console.log(content)
@@ -23,19 +25,23 @@ function Posts({postId}){
 
     return(
         <div>
+           
             <Header subtitle={content.subtitle} title={content.title} imgUrl={content.post_img}/>
-            <article className="mb-4">
+            {token?  <article className="mb-4">
             <div className="container px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
                     <div className="col-md-10 col-lg-8 col-xl-7">
-                        <p>{content.content}</p>
+                        <div dangerouslySetInnerHTML={{__html: content.content}} />
                         <h2 className="section-heading">{content.subtitle}</h2>
                         
                         
                     </div>
                 </div>
             </div>
-        </article>
+        </article>: <div class="position-absolute top-100 start-50 translate-middle mt-5">
+            <button className="btn btn-info"><Link to="/login">You need to login to see this content</Link></button>
+            </div>}
+           
         </div>
     )
 }
---
+++ src/front/js/component/Post Comp/textEditorComp.jsx
@@ -0,0 +1,142 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../../store/appContext";
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
+import Swal from 'sweetalert2'
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../../../styles/textEditor.css"
+
+export default function TextEditor(){
+
+    const { store, actions } = useContext(Context);
+    const [error, setError] = useState('');
+    const navigate = useNavigate()
+
+
+    const [postData, setPostData] = useState({
+        title:"",
+        subtitle: "",
+        image: ""
+    }
+    )
+    const [editorState, setEditorState] = useState(
+        () => EditorState.createEmpty(),
+      )
+
+      const [convertedContent, setConvertedContent] = useState(null);
+
+      const userId = localStorage.getItem("user_id")
+
+      useEffect(() => {
+        let html = convertToHTML(editorState.getCurrentContent());
+        setConvertedContent(html);
+      }, [editorState]);
+    
+     function createMarkup(html) {
+  return {
+    __html: DOMPurify.sanitize(html)
+  }
+}
+
+
+function handleChange(event){
+    const {name,value}= event.target //Destructurar data de formData
+    setPostData(prevPostData =>{
+        return{
+            ...prevPostData, // Traer todo lo que se haya generado por el user
+            [name]: value 
+            
+        }
+    })
+    }
+
+
+
+async function handleSubmit(event){
+    event.preventDefault();
+    const {title, subtitle, image} = postData
+    
+    if(!title || !subtitle || !convertedContent ){
+        setError('Title, content or subtitle is missing')
+    }else{
+        let bodyObj = {
+            title: title,
+            content: createMarkup(convertedContent).__html,
+            subtitle: subtitle,
+            post_img: image,
+            author_id: userId
+        }
+    
+        let response = await actions.genericFetchProtected("create_posts", "POST", bodyObj)
+        let jsonResponse = await response.json()
+
+        if (response.ok) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Great!',
+              text: `${jsonResponse.message}`,
+            })
+            return navigate("/")
+      
+          }else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: `${jsonResponse.msg + " Login again to avoid this error"}`,
+            })
+          }
+        
+    }
+
+    
+
+
+}
+
+
+    return(
+        <div className="container">
+            <div className="main-textEditor">
+
+            <div className="form-floating mb-3">
+                <input required onChange={handleChange} name="title" type="text" className="form-control" id="title" placeholder="name@example.com"/>
+                <label htmlFor="title">Title</label>
+            </div>
+            <div className="form-floating">
+                <input required onChange={handleChange} name="subtitle" type="text" className="form-control" id="subtitle" placeholder="Password"/>
+                <label htmlFor="subtitle">Subtitle</label>
+            </div>
+
+            {error && <p style={{color:"red"}}>{error}</p>}
+
+            <div className="form-floating">
+                <input onChange={handleChange} name="image" type="text" className="form-control" id="image" placeholder="Password"/>
+                <label htmlFor="image">Img URL</label>
+            </div>
+            
+            <div className="mt-5">
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={setEditorState}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
+            />
+            <button onClick={handleSubmit} type="button" className="btn btn-primary my-2">Submit</button>
+            </div>
+           
+            </div>
+
+            
+
+            <div
+                className="preview"
+                dangerouslySetInnerHTML={createMarkup(convertedContent)}>
+            </div>
+        </div>
+    )
+
+}
